2d60c86c1ed112f0ec65ccefd0184761a50595c1
ThheKNG <kkarundu8@gmail.com>
2023-03-13 00:50:59 +0000

    Finish Lab

--- Data-Binding-app/src/app/app.module.ts
+++ Data-Binding-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListcomponentComponent } from './listcomponent/listcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListcomponentComponent
   ],
   imports: [
     BrowserModule,
