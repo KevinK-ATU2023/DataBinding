fc1fff659913c8a59f04726633b279e3fcc2c012
ThheKNG <kkarundu8@gmail.com>
2023-03-13 00:03:19 +0000

    star functions added

--- Data-Binding-app/src/app/app.component.ts
+++ Data-Binding-app/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Data-Binding-app';
+  buttonPresses = 0;
+  showStar = true;
+  
+  buttonPressed() {
+    this.buttonPresses++;
+  }
+
+  lookMessage() {
+    if(this.showStar == true){
+      this.showStar = false;
+    }
+    else {
+      this.showStar = true;
+    }
+  }
+
 }
